7a05e27391297f830c3cd64ac49b763bfde904fd
mi-ck <mi-ck@DESKTOP-1SGFOA7>
2022-03-01 23:09:55 -0600

    creacion de la clase triangle

---
+++ triangle.m
@@ -0,0 +1,15 @@
+classdef triangle
+    %TRIANGLE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        xdata = [0 0.5 1];
+        ydata = [0 1 0];
+        color 
+        g = hgtransform;
+        mass = 1;
+    end
+    
+   
+end
+

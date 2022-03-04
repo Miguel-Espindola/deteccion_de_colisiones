b4ae393718f350aa963955b9d8b1509d9059ce1c
mi-ck <mi-ck@DESKTOP-1SGFOA7>
2022-03-03 19:25:30 -0600

    creacion de la funcion renderer

---
+++ renderer.m
@@ -0,0 +1,13 @@
+function renderer(f)
+%RENDERER: render the scen when you pas a frame
+%   drawnow updates the changes and the boundries of the scen is a square
+%   of 20,20 
+    drawnow
+    plot(f);
+    axis equal
+    xlim([-10,10]);
+    ylim([-10,10]);
+    grid on
+
+end
+
